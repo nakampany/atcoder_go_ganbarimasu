013a5cc249cb34c46477c650f34f21b364e30748
nakampany <nakajima.shota@hrbrain.co.jp>
2023-05-20 21:53:18 +0900

    hogehoge

---
+++ 0520_b/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	var H, W int
+	fmt.Scan(&H, &W)
+
+	arr := make([][]int, H)
+	for i := range arr {
+		arr[i] = make([]int, W)
+		for j := range arr[i] {
+			fmt.Scan(&arr[i][j])
+		}
+
+	words := []string{"s", "n", "u", "k", "e"}
+
+	for i := range arr {
+		for j := range arr[i] {
+			arr[i][j] = words[arr[i][j]]
+		}
+		fmt.Println()
+	}
+
+}
