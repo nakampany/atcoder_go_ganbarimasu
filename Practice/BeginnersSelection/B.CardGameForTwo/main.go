382197b12893fa08de4698a88020a5d9365b6039
naka <nakasyo2792@gmail.com>
2023-06-20 17:14:40 +0900

    hoge

--- Practice/BeginnersSelection/B.CardGameForTwo/main.go
+++ Practice/BeginnersSelection/B.CardGameForTwo/main.go
@@ -1,25 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 func main() {
 	var n int
 	fmt.Scan(&n)
 
-	var card []int
-	fmt.Scan(&card)
+	card := make([]int, n)
+	for i := 0; i < n; i++ {
+		fmt.Scan(&card[i])
+	}
 
 	fmt.Println(card)
 
-	// var alice, bob int
+	sort.Slice(card, func(i, j int) bool { return card[i] > card[j] })
 
-	// for i := 0; i < n; i++ {
-	// 	if i%2 == 0 {
-	// 		bob += card[i]
-	// 	} else {
-	// 		alice += card[i]
-	// 	}
-	// }
+	fmt.Println(card)
 
-	// fmt.Println(bob - alice)
+	var alice, bob int
+
+	for i := 0; i < n; i++ {
+		if i%2 == 0 {
+			fmt.Println("alice")
+			fmt.Println(card[i])
+			alice += card[i]
+		} else {
+			fmt.Println("bob")
+			fmt.Println(card[i])
+			bob += card[i]
+		}
+	}
+
+	fmt.Println(alice - bob)
 }
