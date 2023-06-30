10a4e1c7d16c88d33a265b350060061be1f06e22
naka <nakasyo2792@gmail.com>
2023-06-30 15:38:43 +0900

    hoge

--- 実行環境/main.go
+++ 実行環境/main.go
@@ -2,21 +2,41 @@ package main
 
 import (
 	"fmt"
-	"strings"
+	"math"
 )
 
 func main() {
-	var s string
-	fmt.Scan(&s)
+	var n int
+	fmt.Scan(&n)
 
-	s = strings.ReplaceAll(s, "eraser", "")
-	s = strings.ReplaceAll(s, "erase", "")
-	s = strings.ReplaceAll(s, "dreamer", "")
-	s = strings.ReplaceAll(s, "dream", "")
-
-	if s == "" {
-		fmt.Println("YES")
-	} else {
-		fmt.Println("NO")
+	place := make(map[int][]int)
+	timeKeys := make([]int, n)
+	for i := 0; i < n; i++ {
+		var t, x, y int
+		fmt.Scan(&t, &x, &y)
+		place[t] = []int{x, y}
+		timeKeys[i] = t
 	}
+
+	t0, x0, y0 := 0, 0, 0
+	for _, timeKey := range timeKeys {
+		v := place[timeKey]
+
+		t := timeKey
+		x := v[0]
+		y := v[1]
+
+		if (math.Abs(float64(x0-x)) + math.Abs(float64(y0-y))) > float64(t-t0) {
+			fmt.Println("No")
+			return
+		}
+
+		if int(math.Abs(float64(x-x0))+math.Abs(float64(y-y0))-float64(t-t0))%2 != 0 {
+			fmt.Println("No")
+			return
+		}
+		t0, x0, y0 = t, x, y
+	}
+
+	fmt.Println("Yes")
 }
