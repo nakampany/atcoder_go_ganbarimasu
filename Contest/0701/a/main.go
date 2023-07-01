a1193c4c8a9ace4d16cb6e370eb3aad62b9fafc4
nakampany <nakajima.shota@hrbrain.co.jp>
2023-07-01 23:15:35 +0900

    hogehoge

--- Contest/0701/a/main.go
+++ Contest/0701/a/main.go
@@ -2,12 +2,32 @@ package main
 
 import (
 	"fmt"
-	"strings"
 )
 
 func main() {
-	var n int
-	fmt.Scan(&n)
-	a := make([]int, 7*n)
+	a := make([]int, 8)
+	for i := 0; i < len(a); i++ {
+		fmt.Scan(&a[i])
+	}
 
+	n := len(a) - 1
+	for i := 0; i < n; i++ {
+		if a[i] > a[i+1] {
+			fmt.Println("NO")
+			return
+		}
+		if a[i]%25 != 0 {
+			fmt.Println("NO")
+			return
+		}
+		if a[i] < 100 || a[i] > 675 {
+			fmt.Println("NO")
+			return
+		}
+	}
+	if a[n]%25 != 0 || a[n] < 100 || a[n] > 675 {
+		fmt.Println("NO")
+		return
+	}
+	fmt.Println("YES")
 }
