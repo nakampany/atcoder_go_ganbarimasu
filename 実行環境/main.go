7ed0dcccf42388c324abffef2df33c3378055dce
naka <nakasyo2792@gmail.com>
2023-07-18 13:54:28 +0900

    hoge

--- 実行環境/main.go
+++ 実行環境/main.go
@@ -1,55 +1,34 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
-	"strconv"
+	"sort"
 	"strings"
 )
 
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-	scanner.Scan()
-	split := strings.Split(scanner.Text(), " ")
-	N, _ := strconv.Atoi(split[0])
+	var n int
+	fmt.Scan(&n)
 
-	P := make([]int, N)
-	C := make([]int, N)
-	F := make([][]string, N)
-
-	for i := 0; i < N; i++ {
-		scanner.Scan()
-		split = strings.Split(scanner.Text(), " ")
-
-		P[i], _ = strconv.Atoi(split[0])
-		C[i], _ = strconv.Atoi(split[1])
-
-		F[i] = split[2:]
+	a := make([]string, n)
+	for i := 0; i < n; i++ {
+		fmt.Scan(&a[i])
+		a[i] = sortString(a[i])
 	}
 
-	ans := false
-	for i := 0; i < N; i++ {
-		for j := 0; j < N; j++ {
-			ans = ans || (P[i] >= P[j] && isSuperset(F[j], F[i]) && (P[i] > P[j] || len(F[j]) > len(F[i])))
-		}
+	count := make(map[string]bool)
+	for _, i := range a {
+		count[i] = true
 	}
-	if ans {
-		fmt.Println("Yes")
-	} else {
-		fmt.Println("No")
-	}
+
+	fmt.Println(count)
+
+	// total := len(count) * (len(count) - 1) / 2
+	// fmt.Println(total)
 }
 
-func isSuperset(set1, set2 []string) bool {
-	mapSet1 := make(map[string]bool)
-	for _, val := range set1 {
-		mapSet1[val] = true
-	}
-	for _, val := range set2 {
-		if !mapSet1[val] {
-			return false
-		}
-	}
-	return true
+func sortString(s string) string {
+	slice := strings.Split(s, "")
+	sort.Strings(slice)
+	return strings.Join(slice, "")
 }
