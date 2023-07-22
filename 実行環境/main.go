4443bc489ba70bf83e94517a1faf43ecbae149ef
nakampany <nakajima.shota@hrbrain.co.jp>
2023-07-22 14:09:55 +0900

    hoge

--- 実行環境/main.go
+++ 実行環境/main.go
@@ -4,31 +4,43 @@ import (
 	"fmt"
 	"sort"
 	"strings"
+	"unicode"
 )
 
 func main() {
-	var n int
-	fmt.Scan(&n)
+	var s string
+	fmt.Scan(&s)
 
-	a := make([]string, n)
-	for i := 0; i < n; i++ {
-		fmt.Scan(&a[i])
-		a[i] = sortString(a[i])
+	type Words struct {
+		lower string
+		big   string
 	}
 
-	count := make(map[string]bool)
-	for _, i := range a {
-		count[i] = true
+	words := []Words{}
+	buf := ""      // 大文字が出現するまでの文字を一時的に保持するためのバッファ
+	big_count := 0 // 大文字が出現した回数を数える変数。
+
+	for _, c := range s {
+		buf += string(c)
+		if unicode.IsUpper(c) { // 文字が大文字かどうかをチェック
+			big_count++ // 大文字の出現回数をカウントアップ
+			if big_count%2 == 0 {
+				lower := strings.ToLower(buf)
+				words = append(words, Words{lower, buf})
+				buf = ""
+			}
+		}
 	}
+	// [{fish FisH} {dog DoG} {cat CaT} {aa AA} {aaa AaA} {abc AbC}]
+	// sortしないといけないから、小文字が必要
 
-	fmt.Println(count)
+	sort.Slice(words, func(i, j int) bool {
+		return words[i].lower < words[j].lower
+	})
 
-	// total := len(count) * (len(count) - 1) / 2
-	// fmt.Println(total)
-}
-
-func sortString(s string) string {
-	slice := strings.Split(s, "")
-	sort.Strings(slice)
-	return strings.Join(slice, "")
+	ans := ""
+	for _, p := range words {
+		ans += p.big
+	}
+	fmt.Println(ans)
 }
