d0b37179f1cb8ab5de74e64677b901c39eff3ee6
nakampany <nakajima.shota@hrbrain.co.jp>
2023-07-22 15:48:42 +0900

    hoge

--- Practice/Atcoder入門/5.中級編〜典型を徹底マスター！〜/AB Substrings/main.go
+++ Practice/Atcoder入門/5.中級編〜典型を徹底マスター！〜/AB Substrings/main.go
@@ -1,28 +1,7 @@
 package main
 
-import (
-	"fmt"
-)
+import ()
 
 func main() {
-	var n, y int
-	fmt.Scan(&n, &y)
-
-	check := false
-
-	for i := 0; i <= n; i++ {
-		for j := 0; j <= n-i; j++ {
-			k := n - i - j
-			if 10000*i+5000*j+1000*k == y {
-				fmt.Println(i, j, k)
-				check = true
-				return
-			}
-		}
-	}
-
-	if !check {
-		fmt.Println(-1, -1, -1)
-	}
 
 }
--- Practice/Atcoder入門/5.中級編〜典型を徹底マスター！〜/Otoshidama/main.go
+++ Practice/Atcoder入門/5.中級編〜典型を徹底マスター！〜/Otoshidama/main.go
@@ -1,27 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	var n, y int
 	fmt.Scan(&n, &y)
 
-	var N_1000 int
-	var N_5000 int
-	var N_10000 int
+	check := false
 
 	for i := 0; i <= n; i++ {
-		for j := 0; j <= n; j++ {
+		for j := 0; j <= n-i; j++ {
 			k := n - i - j
-			if 10000*i+5000*j+1000*k == y && k >= 0 {
-				N_10000 = i
-				N_5000 = j
-				N_1000 = k
-				fmt.Println(N_10000, N_5000, N_1000)
+			if 10000*i+5000*j+1000*k == y {
+				fmt.Println(i, j, k)
+				check = true
 				return
 			}
 		}
 	}
 
-	fmt.Println("-1 -1 -1")
+	if !check {
+		fmt.Println(-1, -1, -1)
+	}
+
 }
