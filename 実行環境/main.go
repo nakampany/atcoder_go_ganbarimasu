17deaf2a29f0b9eb314e4b495ee30e8d4647b446
naka <nakasyo2792@gmail.com>
2023-07-23 19:09:18 +0900

    hoge

--- 実行環境/main.go
+++ 実行環境/main.go
@@ -1,34 +1,31 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
-	var n, q int
-	fmt.Scan(&n, &q)
+	var a, b, c, k int
+	fmt.Scan(&a, &b, &c, &k)
 
-	var s string
-	fmt.Scan(&s)
-
-	acc := make([]int, n+1)
-	for i := 1; i < n; i++ {
-		// i番目にACがあるかどうか
-		// ACがあるなら1、ないなら0
-		if s[i-1] == 'A' && s[i] == 'C' {
-			acc[i] = acc[i-1] + 1
-		} else {
-			acc[i] = acc[i-1]
-		}
+	for i := 0; i < k; i++ {
+		a, b, c = sum_number(a, b, c)
 	}
 
-	// fmt.Println(acc)
-	// ACACTACG
-	// [0 1 1 2 2 2 3 3 0]
+	ans := a - b
 
-	for i := 0; i < q; i++ {
-		var l, r int
-		fmt.Scan(&l, &r)
-		fmt.Println(acc[r-1] - acc[l-1])
+	if ans > 1e18 {
+		fmt.Println("Unfair")
+	} else {
+		fmt.Println(ans)
 	}
+}
+
+func sum_number(a, b, c int) (int, int, int) {
+	tmp_a := a
+	tmp_b := b
+	tmp_c := c
+	a = tmp_c + tmp_b
+	b = tmp_a + tmp_c
+	c = tmp_b + tmp_a
+
+	return a, b, c
 }
