5de99d91ccd556eaf47a0e9cc2e1d78a408dbfd2
naka <nakasyo2792@gmail.com>
2023-08-28 09:22:35 +0900

    hoge

--- Contest/0826/d/main.go
+++ Contest/0826/d/main.go
@@ -10,15 +10,5 @@ type Town struct {
 }
 
 func main() {
-	var N, M int
-	fmt.Scan(&N, &M)
-
-	x := make(map[int][]Town)
-	for i := 0; i < M; i++ {
-		var A, B, C int
-		fmt.Scan(&A, &B, &C)
-		x[A] = append(x[A], Town{town: B, road: C})
-		x[B] = append(x[B], Town{town: A, road: C})
-	}
 
 }
