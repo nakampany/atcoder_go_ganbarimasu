aa2632b11b356fcff57a5c3e07c868b28c8e8475
naka <nakasyo2792@gmail.com>
2023-09-08 23:31:59 +0900

    hoge

--- 実行環境/main.go
+++ 実行環境/main.go
@@ -2,49 +2,40 @@ package main
 
 import (
 	"fmt"
+	"sort"
 )
 
-type Town struct {
-	town int
-	road int
-}
-
-var maxDistance int
-var visited []bool
-
-func dfs(graph map[int][]Town, current int, distance int) {
-	if distance > maxDistance {
-		maxDistance = distance
-	}
-	visited[current] = true
-
-	for _, Town := range graph[current] {
-		if !visited[Town.town] {
-			dfs(graph, Town.town, distance+Town.road)
-		}
-	}
-
-	visited[current] = false
-}
-
 func main() {
-	var N, M int
-	fmt.Scan(&N, &M)
+	var n, d, k int
+	var p int64
+	fmt.Scan(&n, &d, &p)
 
-	x := make(map[int][]Town)
-	for i := 0; i < M; i++ {
-		var A, B, C int
-		fmt.Scan(&A, &B, &C)
-		x[A] = append(x[A], Town{town: B, road: C})
-		x[B] = append(x[B], Town{town: A, road: C})
+	f := make([]int64, n)
+	s := make([]int64, n)
+	for i := 0; i < n; i++ {
+		fmt.Scan(&f[i])
 	}
 
-	maxDistance = 0
-	visited = make([]bool, N+1)
+	sort.Slice(f, func(i, j int) bool { return f[i] < f[j] })
 
-	for i := 1; i <= N; i++ {
-		dfs(x, i, 0)
+	s[0] = f[0]
+	for i := 0; i < n-1; i++ {
+		s[i+1] = s[i] + f[i+1]
 	}
 
-	fmt.Println(maxDistance)
+	k = (n + d - 1) / d
+	var ans int64 = p * int64(k)
+
+	for i := 0; i < k; i++ {
+		ans = min(ans, s[int64(n)-1-int64(i)*int64(d)]+(p*int64(i)))
+	}
+
+	fmt.Println(ans)
+}
+
+func min(a, b int64) int64 {
+	if a < b {
+		return a
+	}
+	return b
 }
