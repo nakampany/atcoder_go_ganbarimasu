031432871818da45e75ecf94f7f51b381427343d
naka <nakasyo2792@gmail.com>
2023-09-16 23:24:42 +0900

    hoge

--- 実行環境/main.go
+++ 実行環境/main.go
@@ -2,63 +2,56 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
 func main() {
-	var cells [9]int
-	for i := 0; i < 9; i++ {
-		fmt.Scan(&cells[i])
+	var M int
+	fmt.Scan(&M)
+
+	S := make([]string, 3)
+	for i := 0; i < 3; i++ {
+		fmt.Scan(&S[i])
 	}
 
-	row := [8][3]int{
-		{0, 1, 2}, // 上から 1 行目
-		{3, 4, 5}, // 上から 2 行目
-		{6, 7, 8}, // 上から 3 行目
-		{0, 3, 6}, // 左から 1 列目
-		{1, 4, 7}, // 左から 2 列目
-		{2, 5, 8}, // 左から 3 列目
-		{0, 4, 8}, // 左上から右下
-		{2, 4, 6}} // 右上から左下
+	ans := math.MaxInt64
 
-	// [0 1 2 3 4 5 6 7 8]
-	var order [9]int
-	for i := range order {
-		order[i] = i
-	}
-
-	notDisappoint, all := 0, 0
-
-	permutations(order[:], 0, len(order), func(arr []int) {
-		all++
-		disappointFlag := false
-		for _, r := range row {
-			a, b, c := r[0], r[1], r[2]
-
-			if cells[a] == cells[b] && arr[a] < arr[c] && arr[b] < arr[c] {
-				disappointFlag = true
-			} else if cells[a] == cells[c] && arr[a] < arr[b] && arr[c] < arr[b] {
-				disappointFlag = true
-			} else if cells[b] == cells[c] && arr[b] < arr[a] && arr[c] < arr[a] {
-				disappointFlag = true
+	for i := 0; i < M; i++ {
+		for j := 0; j < M; j++ {
+			for k := 0; k < M/2; k++ {
+				if i != j && i != k && j != k && S[0][i%M] == S[1][j%M] && S[0][i%M] == S[2][k%M] {
+					ans = min(ans, max(i, j, k))
+				}
 			}
 		}
-		if !disappointFlag {
-			notDisappoint++
-		}
-	})
+	}
 
-	fmt.Println(float64(notDisappoint) / float64(all))
+	if ans < math.MaxInt64 {
+		fmt.Println(ans)
+	} else {
+		fmt.Println(-1)
+	}
 }
 
-func permutations(arr []int, l int, r int, callback func([]int)) {
-	if l == r {
-		callback(arr)
-		return
+func max(i, j, k int) int {
+	if i > j {
+		if i > k {
+			return i
+		} else {
+			return k
+		}
+	} else {
+		if j > k {
+			return j
+		} else {
+			return k
+		}
 	}
+}
 
-	for i := l; i < r; i++ {
-		arr[l], arr[i] = arr[i], arr[l]
-		permutations(arr, l+1, r, callback)
-		arr[l], arr[i] = arr[i], arr[l]
+func min(a, b int) int {
+	if a > b {
+		return b
 	}
+	return a
 }
