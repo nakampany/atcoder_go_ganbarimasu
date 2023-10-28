12eb1d30be2b9c2ba499eb329d91c925a2cc3662
nakampany <nakajima.shota@hrbrain.co.jp>
2023-10-28 22:43:21 +0900

    hoge

--- Contest/1014 /b/main.go
+++ Contest/1014 /b/main.go
@@ -5,20 +5,46 @@ import (
 )
 
 func main() {
+
 	var n int
 	fmt.Scan(&n)
 
-	for n%2 == 0 && n > 1 {
-		n /= 2
+	type Base struct {
+		W int
+		X int
+	}
+	bases := make([]Base, n)
+	for i := 0; i < n; i++ {
+		fmt.Scan(&bases[i].W, &bases[i].X)
 	}
 
-	for n%3 == 0 && n > 1 {
-		n /= 3
+	var inter []struct {
+		start, end, W int
 	}
 
-	if n == 1 {
-		fmt.Println("Yes")
-	} else {
-		fmt.Println("No")
+	for _, base := range bases {
+		start := (9 - base.X + 24) % 24
+		end := (18 - base.X + 24) % 24
+		if start <= end {
+			inter = append(inter, struct{ start, end, W int }{start, end, base.W})
+		} else {
+			inter = append(inter, struct{ start, end, W int }{0, end, base.W})
+			inter = append(inter, struct{ start, end, W int }{start, 24, base.W})
+		}
 	}
+
+	maxCount := 0
+	for h := 0; h < 24; h++ {
+		count := 0
+		for _, interval := range inter {
+			if interval.start <= h && h < interval.end {
+				count += interval.W
+			}
+		}
+		if count > maxCount {
+			maxCount = count
+		}
+	}
+
+	fmt.Println(maxCount)
 }
