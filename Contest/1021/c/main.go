88629f4b7c8da2feec92d2612f91db70f002dcc2
nakampany <nakajima.shota@hrbrain.co.jp>
2023-10-28 22:43:36 +0900

    hoge

--- Contest/1021/c/main.go
+++ Contest/1021/c/main.go
@@ -2,48 +2,35 @@ package main
 
 import (
 	"fmt"
+	"sort"
 )
 
 func main() {
+	var n, m int
+	fmt.Scan(&n, &m)
 
-	var h, w int
-	fmt.Scan(&h, &w)
-	grid := make([][]rune, h)
-	visit := make([][]bool, h)
-
-	for i := range grid {
-		var s string
-		fmt.Scan(&s)
-		grid[i] = []rune(s)
-		visit[i] = make([]bool, w)
+	a := make([]int, n)
+	for i := 0; i < n; i++ {
+		fmt.Scan(&a[i])
 	}
 
-	// 上下左右斜め
-	dx := []int{-1, -1, -1, 0, 0, 1, 1, 1}
-	dy := []int{-1, 0, 1, -1, 1, -1, 0, 1}
+	sort.Ints(a)
 
-	// dfs
-	var dfs func(x, y int)
-	dfs = func(x, y int) {
-		if x < 0 || x >= h || y < 0 || y >= w || grid[x][y] == '.' || visit[x][y] {
-			return
-		}
-		visit[x][y] = true
-		for i := 0; i < 8; i++ {
-			nx, ny := x+dx[i], y+dy[i]
-			dfs(nx, ny)
-		}
-	}
-
-	count := 0
-	for i := 0; i < h; i++ {
-		for j := 0; j < w; j++ {
-			if grid[i][j] == '#' && !visit[i][j] {
-				dfs(i, j)
-				count++
+	maxPresents := 0
+	for i := 0; i < n; i++ {
+		low, high := i, n
+		for low < high {
+			mid := (low + high) / 2
+			if a[mid] < a[i]+m {
+				low = mid + 1
+			} else {
+				high = mid
 			}
 		}
+		if maxPresents < low-i {
+			maxPresents = low - i
+		}
 	}
 
-	fmt.Println(count)
+	fmt.Println(maxPresents)
 }
